214986949cfb4996df2dae02a4880533e4bfb7be
1104205c <1104205c@student.gla.ac.uk>
2014-02-12 18:32:30 +0000

    Student class extends User - Adjustment

--- GE2/src/Student.java
+++ GE2/src/Student.java
@@ -1,30 +1,27 @@
+package base;
 
-public class Student {
-	private String studentid;
-	private String name;
-	private int year;
-	public Student(String id,String n,int y){
-		studentid=id;
-		name=n;
-		year=y;
-	}
+import base.User;
 
-	public String getStudentid() {
-		return studentid;
-	}
-	public void setStudentid(String studentid) {
-		this.studentid = studentid;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
+
+public class Student extends User {
+private String studentid;
+private int year;
+public Student(String id,String n,String s,String mail,int y){
+	super(n, s, mail);
+	studentid=id;
+	year=y;
+}
+
+public String getStudentid() {
+	return studentid;
+}
+public void setStudentid(String studentid) {
+	this.studentid = studentid;
+}
+public int getYear() {
+	return year;
+}
+public void setYear(int year) {
+	this.year = year;
+}
 }
