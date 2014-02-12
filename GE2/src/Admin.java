f37ec33d41be9f70958ff9f57d47298d4f6f34df
1104205c <1104205c@student.gla.ac.uk>
2014-02-12 18:55:32 +0000

    Admin Class

---
+++ GE2/src/Admin.java
@@ -0,0 +1,16 @@
+package base;
+
+public class Admin extends User{
+
+	Admin(String n, String s, String mail) {
+		super(n, s, mail);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void createTimetableSlot(){
+	};
+	public void addRoom(TimetableSlot t,Room r){
+		t.assignRoom(r);
+	};
+
+}
