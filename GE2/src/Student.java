fa1650c0538c9d6b638a06fad34f558f0a69f043
1104205c <1104205c@student.gla.ac.uk>
2014-02-12 18:21:27 +0000

    Student class info added

--- GE2/src/Student.java
+++ GE2/src/Student.java
@@ -1,4 +1,30 @@
 
 public class Student {
+	private String studentid;
+	private String name;
+	private int year;
+	public Student(String id,String n,int y){
+		studentid=id;
+		name=n;
+		year=y;
+	}
 
+	public String getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
 }
