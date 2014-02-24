8548b1f43fc676656f5ea9e0d6d3802cd75c9bb9
1104205c <1104205c@student.gla.ac.uk>
2014-02-24 12:56:38 +0000

    changes Timeslot merged with Timetable slot. Adjusted other classes

--- src/test/base/TimetableSlot.java
+++ src/test/base/TimetableSlot.java
@@ -3,30 +3,28 @@ package test.base;
 
 public class TimetableSlot {
 	
-	
-	private Timeslot timeslot;
 	private Room room;
 	private boolean free;
 	private int capacity;
+	private int weekNumber;
+	private String weekDay;
+	private int startTime;
+	private int endTime;
 	
-	
-	public TimetableSlot( Timeslot t){
-		timeslot = t;
-
+	public TimetableSlot(int wn, String wd, int st, int et){
+		weekNumber = wn;
+		weekDay = wd;
+		startTime = st;
+		endTime = et;
 	}
 	
-	public TimetableSlot( Timeslot t,boolean free){
-		this(t);
+	public TimetableSlot(int wn, String wd, int st, int et,boolean free){
+		this(wn,wd,st,et);
 		this.free = free;
 	}
 	
 	
 
-	
-	public Timeslot getTime(){
-		return timeslot;
-	}
-	
 	public Room getRoom(){
 		return room;
 	}
@@ -55,8 +53,24 @@ public class TimetableSlot {
 			return false;
 		}
 	}
+	public int getWeekNum(){
+		return weekNumber;
+	}
+	
+	public String getWeekDay(){
+		return weekDay;
+	}
+	
+	public int getStartTime(){
+		return startTime;
+	}
+	
+	public int getEndTime(){
+		return endTime;
+	}
 	
 	public String toString(){
-		return timeslot.toString();
+		return weekDay+" "+startTime+" "+endTime;
 	}
+	
 }
